349bb394a06d3c4833586afd74a0beab90221145
eralpkor <eralp.ny@gmail.com>
2022-06-10 12:45:07 -0400

    Add data provider code & move files to components directory

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import * as React from "react";
-import { Admin, Resource, EditGuesser } from 'react-admin';
-import { PostList } from './posts';
-import { UserList } from "./users";
-import jsonServerProvider from 'ra-data-json-server';
+import { Admin, Resource } from "react-admin";
+import { PostList, PostEdit, PostCreate } from "./components/Posts";
+import { UserList } from "./components/Users";
+import PostIcon from "@mui/icons-material/Book";
+import UserIcon from "@mui/icons-material/Group";
+import Dashboard from "./components/Dashboard";
+import authProvider from "./Auth/authProvider";
 
-const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+import jsonServerProvider from "ra-data-json-server";
+// import dataProvider from "./DataProvider/dataProvider";
+
+const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
 
 const App = () => (
-  <Admin dataProvider={dataProvider}>
-    <Resource name="posts" list={PostList} edit={EditGuesser} />
+  <Admin
+    dashboard={Dashboard}
+    authProvider={authProvider}
+    dataProvider={dataProvider}
+  >
+    <Resource
+      name="posts"
+      list={PostList}
+      edit={PostEdit}
+      create={PostCreate}
+      icon={PostIcon}
+    />
     {/* <Resource name='posts' list={ListGuesser} /> */}
     {/* ListGuesser guesses the format to use */}
     {/* for the columns of the list based on the data fetched from the API */}
-    <Resource name='users' list={UserList} />
+    <Resource name="users" list={UserList} icon={UserIcon} />
   </Admin>
-)
+);
 
 export default App;
